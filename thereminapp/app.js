ad4b44d8bbdb584f8018a94df11e59bb4ddf06ba
axeralater <trsei21@yahoo.co.jp>
2015-07-04 14:44:13 +0900

    css,js変更

--- thereminapp/app.js
+++ thereminapp/app.js
@@ -1,3 +1,23 @@
 window.addEventListener("load", function() {
-  console.log("Hello World!");
+  var player = document.getElementById("player");
+  var button = document.getElementById("button");
+  button.addEventListener("click",reWrite);
+  //var num;
+function reWrite(){
+  if(player.className == "play"){
+    player.className = "pause";
+    button.value = "Play"
+  }else if(player.className == "pause"){
+    player.className = "play";
+    button.value = "Pause"
+  }else{
+    console.log("エラー");
+  }
+}
+  
+  var pause = document.getElementById("pause");
+  var button = document.getElementById("button");
+  button.addEventListener("click",reWrite);
+  //var num;
+
 });
