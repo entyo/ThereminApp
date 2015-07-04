328ee4c9628576908c68d151c4b0011f82cd81fe
tomonari_t <tomo1208.japan@gmail.com>
2015-07-04 16:07:34 +0900

    img出し変え実装

--- thereminapp/js/app.js
+++ thereminapp/js/app.js
@@ -1,5 +1,37 @@
 var app = angular.module("Theremin",[]);
 
-app.controller("MainCtr",[function(){
+app.controller("MainCtr",["$scope",function($scope){
+    //値によってdisplays
+    $scope.isHighShow = false;
+    $scope.isHigherShow = false;
+    $scope.isLowShow = false;
+    $scope.isLowerShow = false;
+    function allFalse(){
+        $scope.isHighShow = false;
+        $scope.isHigherShow = false;
+        $scope.isLowShow = false;
+        $scope.isLowerShow = false;
+    }
+    $scope.$watch("waveLevel",function(newVal,oldVal){
+        allFalse();
+        console.log(newVal);
+        var _newVal = +newVal;
+        switch (_newVal) {
+            case 4:
+                $scope.isHighShow = true;
+                console.log(newVal);
+                break;
+            case 3:
+                $scope.isHigherShow = true;
+                break;
+            case 2:
+                $scope.isLowerShow = true;
+                break;
+            case 1:
+                $scope.isLowShow = true;
+                break;
+            default:
 
+        }
+    });
 }]);
